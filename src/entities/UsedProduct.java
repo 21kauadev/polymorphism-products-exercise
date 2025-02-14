d9bc43456f94de4de19990343030edd0f2751378
21kauadev <kauantnetsoares@gmail.com>
2025-02-14 10:25:36 -0300

    feat: add UsedProduct class with polymorphism

---
+++ src/entities/UsedProduct.java
@@ -0,0 +1,25 @@
+package entities;
+
+import java.time.LocalDate;
+
+public class UsedProduct extends Product {
+    private LocalDate manufactureDate;
+
+    public UsedProduct() {
+        super();
+    }
+
+    public UsedProduct(String name, Double price, LocalDate manufactureDate) {
+        super(name, price);
+        this.manufactureDate = manufactureDate;
+    }
+
+    public LocalDate getManufactureDate() {
+        return manufactureDate;
+    }
+
+    @Override
+    public String priceTag() {
+        return super.priceTag() + String.format("USED PRODUCT (Manufacture Date: %s)", this.manufactureDate);
+    }
+}
